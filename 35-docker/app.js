cbb93de44af301cef1d67893820fa57ea16c6e23
shaharsol <shahar@gnift.com>
2026-08-23 12:16:56 +0300

    dockerfile

---
+++ 35-docker/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const axios = require('axios')
+const { toXML } = require('jstoxml')
+
+const app = express()
+
+const loadUsers = async (request, response, next) => {
+    const res = await axios('https://dummyjson.com/users')
+    const { users } = res.data
+    request.users = users
+    next()
+}
+
+const filterUsers = async (request, response, next) => {
+    request.filtered = request.query.filter ? request.users.filter(user => user.firstName.includes(request.query.filter)) : [...request.users]
+    next()
+}
+
+const respond = async (request, response) => {
+    if (request.query.format === 'xml') {
+        response.setHeader('Content-Type', 'application/xml')
+        response.send(toXML(request.filtered))
+    } else {
+        response.json(request.filtered)
+    }
+}
+
+app.get('/', loadUsers, filterUsers, respond)
+
+
+app.listen(3000, () => { console.log('started...') })
---
+++ 35-docker/readme.md
@@ -0,0 +1,7 @@
+create an express app that
+fetches the users list from https://dummyjson.com/users
+filter names according to a query param `filter`
+returns either json or xml depending on query param `format` (default json)
+
+
+get query params in express : request.query.filter, request.query.format
