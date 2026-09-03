fd778863b3b637d3a06cec8b90b5fe30274244c1
shaharsol <shahar@gnift.com>
2026-09-03 10:34:10 +0300

    typeorm, repos etc

--- 41-database/src/users/user.entity.ts
+++ 41-database/src/users/user.entity.ts
@@ -13,4 +13,7 @@ export class User {
 
   @Column({ default: true })
   isActive!: boolean;
+
+  @HasMany(Product)
+  products: Product[]
 }
---
+++ 41-database/src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+}
